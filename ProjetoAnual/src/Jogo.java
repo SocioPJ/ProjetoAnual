c438959418433c5c070883200548876b5219b03f
SocioPJ <20.00704-3@maua.br>
2022-05-26 22:23:41 -0300

    Feito classe Jogo

--- ProjetoAnual/src/Jogo.java
+++ ProjetoAnual/src/Jogo.java
@@ -1,3 +1,34 @@
-public class Jogo {
+public class Jogo implements IGerarDesconto {
+    private final String nome;
+    private final EnumTipoDeJogo tipoJogo;
+    private final int quantidadeDeJogadores;
+    private final String descricao;
+    private final double preco;
+    public Jogo(String nome, EnumTipoDeJogo tipoJogo, int quantidadeDeJogadores, String descricao, double preco) {
+        this.nome = nome;
+        this.tipoJogo = tipoJogo;
+        this.quantidadeDeJogadores = quantidadeDeJogadores;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
     
+    public String getNome() {
+        return nome;
+    }
+    public EnumTipoDeJogo getTipoJogo() {
+        return tipoJogo;
+    }
+    public int getQuantidadeDeJogadores() {
+        return quantidadeDeJogadores;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    @Override
+    public double gerarDesconto(Jogo jogo) {
+        return getPreco()*0.9;
+    }
 }
