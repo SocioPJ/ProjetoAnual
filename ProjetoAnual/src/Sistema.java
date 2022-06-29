0e51c02fa68693317b78eab49426ddef831f683d
SocioPJ <20.00704-3@maua.br>
2022-06-29 14:45:09 -0300

    gerador CPF

--- ProjetoAnual/src/Sistema.java
+++ ProjetoAnual/src/Sistema.java
@@ -1,7 +1,12 @@
 public class Sistema {
     public static void run(){
-        Usuario usuario1 = new Usuario("João", "jpsocio45@gmail.com", "01/01/2000", 1180882687);
+        Usuario usuario1 = new Usuario("João", "jpsocio45@gmail.com", "01/01/2000");
         Conta conta1 = new Conta(usuario1, "senha12355");
+        Usuario usuario2 = new Usuario("Maria","gordolazuado@spotcred.com.br", "09/10/1985");
+        Conta conta2 = new Conta(usuario2, "fefe6969xoxo");
         System.out.println(conta1);
+        System.out.println(conta2);
+        System.out.println("\n");
+        
     }
 }
--- ProjetoAnual/src/Usuario.java
+++ ProjetoAnual/src/Usuario.java
@@ -1,13 +1,15 @@
+import java.util.Random;
+
 public class Usuario {
     private String nome_usuario;
     private String email_usuario;
     private final String data_nascimento;
-    private final int cpf;
-    public Usuario(String nome_usuario, String email_usuario, String data_nascimento, int cpf) {
+    private int cpf = Usuario.gerarCPF();
+    public Usuario(String nome_usuario, String email_usuario, String data_nascimento) {
         this.nome_usuario = nome_usuario;
         this.email_usuario = email_usuario;
         this.data_nascimento = data_nascimento;
-        this.cpf = cpf;
+        
     }
     public String getNome_usuario() {
         return nome_usuario;
@@ -21,15 +23,28 @@ public class Usuario {
     public int getCpf() {
         return cpf;
     }
+    public void setNome_usuario(String nome_usuario) {
+        this.nome_usuario = nome_usuario;
+    }
+    public void setEmail_usuario(String email_usuario) {
+        this.email_usuario = email_usuario;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+
+    public static int gerarCPF() {
+        Random random = new Random();
+        int gerador = random.nextInt(90000000);
+        return gerador;
+    }
+   
     @Override
     public String toString() {
         return "" + nome_usuario
                 + "\nEmail : " + email_usuario
                 + "\nData de nascimento : " + data_nascimento
                 + "\nCPF : " + cpf;
-    }
-
-    public int gerarCPF() {
-        return cpf;
     }
 }
